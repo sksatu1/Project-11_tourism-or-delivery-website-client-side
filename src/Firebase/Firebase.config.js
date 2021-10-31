393d0e2c726bdc86983ae50f07f952d6f02e998e
sksatu1 <satueee18@gmail.com>
2021-10-31 19:52:01 +0600

    13th

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyCwALQJtpFVB2JpDRBrv7TdAP4upE-3Hec",
-  authDomain: "sk-travel-427e8.firebaseapp.com",
-  projectId: "sk-travel-427e8",
-  storageBucket: "sk-travel-427e8.appspot.com",
-  messagingSenderId: "410899946228",
-  appId: "1:410899946228:web:4d1aa8ac55de80aaa1dca1"
+  apiKey: "AIzaSyB3XxwEaSEjNIYW5O0YI1TosVb5mhBe5gQ",
+  authDomain: "sk-travel-assignment-11.firebaseapp.com",
+  projectId: "sk-travel-assignment-11",
+  storageBucket: "sk-travel-assignment-11.appspot.com",
+  messagingSenderId: "910219123310",
+  appId: "1:910219123310:web:fa46c219ce757a21ac647f"
 };
 export default firebaseConfig;
